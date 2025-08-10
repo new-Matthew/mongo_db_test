a56c07f08f953b5dee94a44503520508277014a8
matheus <matheusrodrig16@gmail.com>
2025-08-10 19:01:15 -0300

    feat: first database connection

--- extract_and_save_file.py
+++ extract_and_save_file.py
@@ -6,29 +6,38 @@ load_dotenv()
 
 uri = os.getenv("MONGODB_URI")
 
-# Verifica se a URI foi carregada
 if uri is None:
     print("Erro: A variável de ambiente MONGODB_URI não foi encontrada.")
 else:
-    # Cria uma conexão com o MongoDB Atlas
+
     client = MongoClient(uri)
 
     try:
-        # Acessa uma base de dados (exemplo: 'admin') para testar a conexão
-        db = client.admin
 
-        # O comando ismaster é uma forma de checar a conexão
+        db = client["db_products"]
+        collection = db["products"]
+        client.list_database_names()
+
+
         server_info = db.command("ismaster")
 
         print("Conexão com o MongoDB Atlas estabelecida com sucesso!")
+        product = {"product": "computer", "amount": 77}
+        # 1. Armazena o resultado da inserção em uma variável
+        insert_result = collection.insert_one(product)
 
-        # Aqui você pode começar a usar o seu cliente
-        # db = client.seu_banco_de_dados
-        # collection = db.sua_colecao
+        # 2. Usa a variável para acessar a ID do documento inserido
+        inserted_id = insert_result.inserted_id
 
+        print(f"Documento inserido com sucesso! ID: {inserted_id}")
+
+        # 3. Usa a ID correta para buscar o documento
+        inserted_product = collection.find_one({"_id": inserted_id})
+
+        print("\nDocumento encontrado no banco de dados:")
+        print(inserted_product)
     except Exception as e:
         print(f"Erro ao conectar ao MongoDB Atlas: {e}")
 
     finally:
-        # Feche a conexão quando não precisar mais dela
         client.close()
